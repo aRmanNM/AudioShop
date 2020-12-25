98da2842a3fd23268f665df4518172d6dfa95be6
DonAlex <kaviyani.ma@gmail.com>
2020-12-25 17:10:43 +0330

    Added Zarinpal Test part1

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -6,6 +6,8 @@ using API.Entities;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ZarinpalSandbox;
 
 namespace API.Controllers
 {
@@ -15,14 +17,18 @@ namespace API.Controllers
     public class OrderController : ControllerBase
     {
         private readonly IOrderService _orderService;
-        public OrderController(IOrderService orderService)
+        private readonly IConfiguration _config;
+        public OrderController(IOrderService orderService, IConfiguration config)
         {
+            _config = config;
             _orderService = orderService;
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreateOrder(BasketDto basketDto){
-            var order = new Order{
+        public async Task<ActionResult> CreateOrder(BasketDto basketDto)
+        {
+            var order = new Order
+            {
                 UserId = basketDto.UserId,
                 TotalPrice = basketDto.TotalPrice,
                 Status = false,
@@ -31,7 +37,8 @@ namespace API.Controllers
 
             await _orderService.CreateOrder(order);
 
-            var basketItems = basketDto.CourseDtos?.Select(c => new BasketItem {
+            var basketItems = basketDto.CourseDtos?.Select(c => new BasketItem
+            {
                 OrderId = order.Id,
                 CourseId = c.Id,
                 Price = c.Price
@@ -39,9 +46,29 @@ namespace API.Controllers
 
             await _orderService.CreateBasketItems(basketItems);
 
-            return Ok();
+            return RedirectToAction(nameof(PayOrder), order);
         }
 
+        [HttpPost("payorder")]
+        public ActionResult PayOrder(Order order)
+        {
+            if (order.Status)
+            {
+                return BadRequest();
+            }
 
+            var payment = new Payment(order.TotalPrice);
+            var result = payment.PaymentRequest($"پرداخت فاکتور شماره {order.Id}",
+                _config["ApiUrl"] + "Order/PaymentResult/" + order.Id);
+
+            if(result.Result.Status == 100)
+            {
+                return Redirect("https://sandbox.zarinpal.com/pg/StartPay/" + result.Result.Authority);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
     }
 }
