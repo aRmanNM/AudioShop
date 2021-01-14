9f5ff8b4d28a3993052c311c79f641ed734019f0
armannm <anm4500@gmail.com>
2021-01-14 23:05:42 +0330

    invitation code added

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -6,7 +6,7 @@ namespace Core.Entities
 {
     public class User : IdentityUser
     {
-        public string DisplayName { get; set; }
+        public string InvitationCode { get; set; }
         public string VerificationToken { get; set; }
         public ICollection<UserRole> UserRoles { get; set; }
         public ICollection<Order> Orders { get; set; }
