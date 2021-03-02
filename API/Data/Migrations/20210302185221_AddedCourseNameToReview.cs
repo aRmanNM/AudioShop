7189e2b2a958a1f6361534f2bf6bde4c7cd90c63
armannm <anm4500@gmail.com>
2021-03-02 22:24:36 +0330

    fixed migration error

---
+++ API/Data/Migrations/20210302185221_AddedCourseNameToReview.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class AddedCourseNameToReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SliderItems_Courses_CourseId",
+                table: "SliderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SliderItems_CourseId",
+                table: "SliderItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseName",
+                table: "Reviews",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseName",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SliderItems_CourseId",
+                table: "SliderItems",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SliderItems_Courses_CourseId",
+                table: "SliderItems",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
