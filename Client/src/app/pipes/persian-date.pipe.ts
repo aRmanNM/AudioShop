d32b4fc467059176165b7ab9762ec6ecd851e6cd
armannm <anm4500@gmail.com>
2021-04-21 00:16:04 +0430

    date pipe improved

--- Client/src/app/pipes/persian-date.pipe.ts
+++ Client/src/app/pipes/persian-date.pipe.ts
@@ -1,9 +1,14 @@
 import * as jalaliMoment from 'jalali-moment';
 import {Pipe, PipeTransform} from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Pipe({name: 'PersianDate'})
 export class PersianDatePipe implements PipeTransform {
-  transform(value: string, exponent?: number): any {
-    return jalaliMoment(value, 'YYYY/MM/DD').format('jYYYY/jMM/jDD');
+  transform(value: string, withTime: boolean = true): any {
+    if (withTime) {
+      return jalaliMoment(formatDate(value, 'yyyy/MM/dd - H:mm', 'en-US', '+0430'), 'YYYY/MM/DD - H:mm').format('jYYYY/jMM/jDD - H:mm');
+    } else {
+      return jalaliMoment(formatDate(value, 'yyyy/MM/dd', 'en-US', '+0430'), 'YYYY/MM/DD').format('jYYYY/jMM/jDD');
+    }
   }
 }
